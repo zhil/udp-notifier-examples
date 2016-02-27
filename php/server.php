a7f0fab0b34adaaf46eafa02912a325fb826921e
Zhil <starsfm@gmail.com>
2016-02-27 20:06:25 +0200

    connector2 prototype

--- php/server.php
+++ php/server.php
@@ -3,5 +3,9 @@ require_once "vendor/autoload.php";
 
 $listener = new \Zhil\UdpNotifier\Listener("localhost","1234","secret");
 $listener->run(function($message,$address) {
-    echo 'client ' . $address . ': ' . $message . PHP_EOL;
+    if(substr($message,0,1) != "{") {
+        // all messages are json_encoded. Probably, secret is not valid.
+        echo 'Invalid secret? cant encode json'.PHP_EOL;
+    }
+    echo 'client '.$address.': '.$message.PHP_EOL;
 });
